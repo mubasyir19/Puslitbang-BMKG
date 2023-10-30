082bb76f5ce63536305b566e7dede00d88e2e59f
GunturMif <gunturmiftah8@gmail.com>
2023-10-30 16:41:33 +0700

    fixing

--- src/app/(product)/klimatologi/page.js
+++ src/app/(product)/klimatologi/page.js
@@ -3,7 +3,3 @@ import { redirect } from 'next/navigation'
 export default function Klimatologi() {
   redirect('/klimatologi/inarcm')
 }
-
-export default function Profile() {
-  redirect('/profile')
-}
