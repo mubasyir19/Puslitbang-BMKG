347f5497acc58114374daf0551931eb0f3e8c4fe
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-11-14 14:47:32 +0700

    update(dashboard): update layout dashboar

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,6 +1,5 @@
 import { Poppins } from 'next/font/google'
 import '../globals.css'
-import { Components } from '@/components'
 
 const poppins = Poppins({
   weight: ['300', '400', '500', '600', '700'],
@@ -17,16 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <div className="flex h-screen">
-          <Components.SidebarDashboard />
-          <div className="flex-1 flex flex-col overflow-hidden">
-            <Components.NavbarDashboard />
-            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4">
-              {children}
-            </main>
-            <Components.FooterDashboard />
-          </div>
-        </div>
+        <main>{children}</main>
       </body>
     </html>
   )
